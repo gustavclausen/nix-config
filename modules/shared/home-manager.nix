0fd7d33db9cd4dc89c8024ad8ddcfa82af3abf4a
gustavclausen <gustav.clausen@zeronorth.com>
2024-07-28 12:12:33 +0200

    fix(lazygit): edit/open commands with tmux

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -351,7 +351,8 @@
 
     settings = {
       os = {
-        edit = "nvim --server /tmp/nvim-server-$(tmux display-message -p '\#{session_id}-#{window_id}-#{pane_id}').pipe --remote-send \"<cmd>lua require('core.scripts.lazygit-open-file')('{{filename}}', '{{line}}')<CR>\"";
+        edit = "nvim --server /tmp/nvim-server-$(tmux display-message -p '\#{session_id}-#{window_id}-#{pane_id}').pipe --remote-send \"<cmd>lua require('core.scripts.lazygit-open-file')('{{filename}}')<CR>\"";
+        editAtLine = "nvim --server /tmp/nvim-server-$(tmux display-message -p '\#{session_id}-#{window_id}-#{pane_id}').pipe --remote-send \"<cmd>lua require('core.scripts.lazygit-open-file')('{{filename}}', '{{line}}')<CR>\"";
         open = "nvim --server /tmp/nvim-server-$(tmux display-message -p '\#{session_id}-#{window_id}-#{pane_id}').pipe --remote-send \"<cmd>lua require('core.scripts.lazygit-open-file')('{{filename}}', '{{line}}')<CR>\"";
       };
       keybinding = {
