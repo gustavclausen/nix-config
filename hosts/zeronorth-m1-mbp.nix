a644ea2251918356364d85ec76cb83e8d784fe46
gustavclausen <gustav.clausen@zeronorth.com>
2024-07-28 13:40:30 +0200

    fix(zn): malformed brew formulaes

--- hosts/zeronorth-m1-mbp.nix
+++ hosts/zeronorth-m1-mbp.nix
@@ -20,11 +20,7 @@
         name = "argoproj/tap";
       }
     ];
-    brews = pkgs.callPackage ../modules/darwin/brews.nix {extra = [
-      {
-        name = "argoproj/tap/kubectl-argo-rollouts";
-      }
-    ];
+    brews = pkgs.callPackage ../modules/darwin/brews.nix {extra = ["argoproj/tap/kubectl-argo-rollouts"];};
   };
 
   home-manager = {
