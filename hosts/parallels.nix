be9be3e590043b40f1343653c15740660160f410
gustavclausen <gustav@gustavclausen.com>
2024-12-29 13:14:27 +0100

    wip

--- hosts/parallels.nix
+++ hosts/parallels.nix
@@ -1,23 +1,19 @@
-{pkgs, ...}: {
-  home = rec {
-    username = "gustavclausen";
-    homeDirectory = "/home/${username}";
+{
+  pkgs,
+  currentSystemUser,
+  homePath,
+}: {
+  imports = [
+    ./default.nix
+  ];
+
+  home = {
+    username = currentSystemUser;
+    homeDirectory = homePath;
     stateVersion = "25.05";
     packages = with pkgs; [
       neofetch
     ];
   };
   programs.home-manager.enable = true;
-  nix = {
-    enable = true;
-    package = pkgs.nix;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-  # programs.git = {
-  #  enable = true;
-  #  userName = "Jane Doe";
-  #  userEmail = "jane.doe@example.org";
-  #};
 }
