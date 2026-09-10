9949716a5abee01daaac9774eb5892ff43cd689e
gustavclausen <gustav@gustavclausen.com>
2026-09-10 15:51:21 +0200

    feat(macbook): add "storing-personal-skills"

--- hosts/personal-macbook-pro-m5/default.nix
+++ hosts/personal-macbook-pro-m5/default.nix
@@ -78,7 +78,7 @@
               claude-code.enable = true;
             };
             skills.enableAll = ["superpowers" "mattpocock-skills"];
-            skills.enable = ["installing-packages-via-nix"];
+            skills.enable = ["installing-packages-via-nix" "storing-personal-skills"];
           };
           git = {
             enable = true;
